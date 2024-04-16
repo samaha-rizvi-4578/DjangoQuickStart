678559ff98cd32c5ea5f136ff225ab90a99e76c0
rizvi <k214578@nu.edu.pk>
2024-04-16 12:28:14 +0500

    removing hardcoded urls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     # /polls/
     path('', views.index, name='index'),
 #     /polls/5/
-    path("<int:question_id>/", views.detail, name='detail'),
+#     added the word specifes hehehe
+    path("specifies/<int:question_id>/", views.detail, name='detail'),
 #     /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
 #     /polls/5/vote/
