396a10fbcc6907a5a78ed64592313718de166770
rizvi <k214578@nu.edu.pk>
2024-04-16 11:59:00 +0500

    creating views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /polls/
     path('', views.index, name='index'),
+#     /polls/5/
+    path("<int:question_id>/", views.detail, name='detail'),
+#     /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+#     /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+
 ]
